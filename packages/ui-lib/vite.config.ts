bad106e9e8226c6f71921f23442c52e1251716a9
Yorick Toma <yoricktoma@gmail.com>
2023-10-03 23:42:49 +0200

    Dupe import

--- packages/ui-lib/vite.config.ts
+++ packages/ui-lib/vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { glob } from 'glob'
 import { fileURLToPath } from 'node:url'
-import { extname, relative, resolve, resolve } from 'path'
+import { extname, relative, resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
