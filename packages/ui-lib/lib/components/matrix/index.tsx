5add99fd9c4a3fda031d6e362723a1d84f67c9df
Yorick Toma <yoricktoma@gmail.com>
2023-10-07 00:52:14 +0200

    Something along these lines

--- packages/ui-lib/lib/components/matrix/index.tsx
+++ packages/ui-lib/lib/components/matrix/index.tsx
@@ -2,34 +2,72 @@ import styles from './styles.module.css'
 import { useRef, useEffect, useState } from 'react'
 
 export const Matrix: React.FC = () => {
-    const containerRef = useRef<HTMLDivElement>(document.createElement('div'))
+    const containerRef = useRef(null)
+    const dotSize = 10
     const [totalPixels, setTotalPixels] = useState(0)
 
-    useEffect(() => {
-        const calc =
-            (containerRef.current.offsetWidth *
-                containerRef.current.offsetHeight) /
-            128
-        setTotalPixels(calc)
+    const [letters, setLetters] = useState([])
 
-        console.log(calc)
+    const [containerState, setContainerState] = useState({
+        width: 350,
+        height: 100,
+    })
+
+    useEffect(() => {
+        setContainerState({
+            width: containerRef.current.offsetWidth,
+            height: containerRef.current.offsetHeight,
+        })
+
+        setTotalPixels(
+            ((containerState.width / dotSize) * containerState.height) / dotSize
+        )
+
+        // w/e im tired sue me
+        setTimeout(() => {
+            setLetters([
+                [316, 281, 246, 211, 176, 247, 248, 249, 214, 179, 284, 319],
+                [321, 286, 251, 216, 181],
+            ])
+        }, 1500)
     }, [])
 
     return (
         <div
-            ref={containerRef}
             onClick={() => {
-                console.log('joe')
-                const calc =
-                    containerRef.current.offsetWidth *
-                    containerRef.current.offsetHeight
-                setTotalPixels(calc)
+                setTotalPixels(
+                    ((containerState.width / dotSize) * containerState.height) /
+                        dotSize
+                )
+                console.log(totalPixels)
             }}
+            ref={containerRef}
             className={styles.gridContainer}
         >
             <div className={styles.matrix}>
                 {Array.from({ length: totalPixels }, (_, i) => {
-                    return <div key={i} className={styles.matrixdot} />
+                    const isLetter = letters.flat().includes(i)
+                    const realDotStyle = {
+                        backgroundColor: isLetter ? 'white' : 'black',
+                        transition: 'background-color 2s ease',
+                    }
+
+                    return (
+                        <div
+                            style={{
+                                width: `${dotSize}px`,
+                                height: `${dotSize}px`,
+                            }}
+                            matrixId={i}
+                            key={i}
+                            className={styles.matrixdot}
+                        >
+                            <span
+                                className={styles.realDot}
+                                style={realDotStyle}
+                            />
+                        </div>
+                    )
                 })}
             </div>
         </div>
