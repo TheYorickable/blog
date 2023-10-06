2e2101788d6f476589db3bdbcb77c1c6fa3fe315
Yorick Toma <yoricktoma@gmail.com>
2023-10-07 01:09:24 +0200

    This looks better but is still off

--- packages/ui-lib/lib/components/matrix/index.tsx
+++ packages/ui-lib/lib/components/matrix/index.tsx
@@ -6,30 +6,23 @@ export const Matrix: React.FC = () => {
     const dotSize = 10
     const [totalPixels, setTotalPixels] = useState(0)
 
-    const [letters, setLetters] = useState([])
-
     const [containerState, setContainerState] = useState({
-        width: 350,
+        width: 100,
         height: 100,
     })
 
     useEffect(() => {
+        const w = containerRef.current.offsetWidth
+        const h = containerRef.current.offsetHeight
+
         setContainerState({
-            width: containerRef.current.offsetWidth,
-            height: containerRef.current.offsetHeight,
+            width: w,
+            height: h,
         })
 
         setTotalPixels(
             ((containerState.width / dotSize) * containerState.height) / dotSize
         )
-
-        // w/e im tired sue me
-        setTimeout(() => {
-            setLetters([
-                [316, 281, 246, 211, 176, 247, 248, 249, 214, 179, 284, 319],
-                [321, 286, 251, 216, 181],
-            ])
-        }, 1500)
     }, [])
 
     return (
@@ -46,7 +39,7 @@ export const Matrix: React.FC = () => {
         >
             <div className={styles.matrix}>
                 {Array.from({ length: totalPixels }, (_, i) => {
-                    const isLetter = letters.flat().includes(i)
+                    const isLetter = false
                     const realDotStyle = {
                         backgroundColor: isLetter ? 'white' : 'black',
                         transition: 'background-color 2s ease',
